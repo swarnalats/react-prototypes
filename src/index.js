67edab28a033df150237d8ff282de1a8fd481444
Swarnalata Keshavan <swarna_latha@hotmail.com>
2019-06-14 13:19:46 -0700

    Prototype React Intro Swarnalata Keshavan

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const LuckyNumber = () => Math.floor(Math.random()*(1000-1+1)+1) ;
+
+const userObj = {
+    name:"Swarna",
+    luckyNumber:LuckyNumber()
+};
+
+console.log("LuckyNUmber is:", userObj.luckyNumber);
+const Greeting = (props) => { 
+    console.log("Typeof:" + typeof(props));
+    return (<div className="container">
+        <h1>Hello {props.name}</h1>          
+        <h2 className="text-muted">Your lucky number is: {props.luckyNumber}</h2>
+    </div>
+    )
+};
+
 ReactDOM.render(
-    <h1 className="container text-center">React Prototypes!</h1>,
+    <Greeting userObj />,
     document.getElementById('root')
 );
